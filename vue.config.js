7d70c54554e2d019c1752687b899691e9e9274d8
murphyyi <zy84338719@hotmail.com>
2020-06-08 03:07:33 +0800

    修改vue.config.js 部署路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// eslint-disable-next-line no-unused-vars
 const webpack = require('webpack');
 const UglifyPlugin = require("uglifyjs-webpack-plugin");
 
@@ -7,51 +8,52 @@ module.exports = {
     // 基本路径
     publicPath: './',
     // 输出文件目录
-    outputDir: 'docs',
+    outputDir: 'public',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
     // webpack配置
     // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-    chainWebpack: () => {},
+    chainWebpack: () => {
+    },
     configureWebpack: (config) => {
         if (process.env.NODE_ENV === 'production') {
-          // 为生产环境修改配置...
-          config.mode = 'production';
-		  // 将每个依赖包打包成单独的js文件
-		  let optimization = {
-			runtimeChunk: 'single',
-			splitChunks: {
-			  chunks: 'all',
-			  maxInitialRequests: Infinity,
-			  minSize: 20000, // 依赖包超过20000bit将被单独打包
-			  cacheGroups: {
-				vendor: {
-				  test: /[\\/]node_modules[\\/]/,
-				  name (module) {
-					// get the name. E.g. node_modules/packageName/not/this/part.js
-					// or node_modules/packageName
-					const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-					// npm package names are URL-safe, but some servers don't like @ symbols
-					return `npm.${packageName.replace('@', '')}`
-				  }
-				}
-			  }
-			},
-			   minimizer: [
-				  new UglifyPlugin({
-					uglifyOptions: {
-					  compress: {
-						drop_console: true, // console
-						drop_debugger: false,
-						pure_funcs: ["console.log"] // 移除console
-					  }
-					}
-				  })
-			]
-		  }
-		  Object.assign(config, {
-			optimization
-		  })
+            // 为生产环境修改配置...
+            config.mode = 'production';
+            // 将每个依赖包打包成单独的js文件
+            let optimization = {
+                runtimeChunk: 'single',
+                splitChunks: {
+                    chunks: 'all',
+                    maxInitialRequests: Infinity,
+                    minSize: 20000, // 依赖包超过20000bit将被单独打包
+                    cacheGroups: {
+                        vendor: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name(module) {
+                                // get the name. E.g. node_modules/packageName/not/this/part.js
+                                // or node_modules/packageName
+                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                                // npm package names are URL-safe, but some servers don't like @ symbols
+                                return `npm.${packageName.replace('@', '')}`
+                            }
+                        }
+                    }
+                },
+                minimizer: [
+                    new UglifyPlugin({
+                        uglifyOptions: {
+                            compress: {
+                                drop_console: true, // console
+                                drop_debugger: false,
+                                pure_funcs: ["console.log"] // 移除console
+                            }
+                        }
+                    })
+                ]
+            }
+            Object.assign(config, {
+                optimization
+            })
         } else {
             // 为开发环境修改配置...
             config.mode = 'development';
@@ -60,7 +62,7 @@ module.exports = {
         Object.assign(config, {
             // 开发生产共同配置
             resolve: {
-				extensions: ['.js', '.vue', '.json'],
+                extensions: ['.js', '.vue', '.json'],
                 alias: {
                     '@': path.resolve(__dirname, './src'),
                     '@c': path.resolve(__dirname, './src/components')
@@ -105,7 +107,9 @@ module.exports = {
         //      }
         //  }
         // },
-        before: (app) => {}
+        // eslint-disable-next-line no-unused-vars
+        before: (app) => {
+        }
     },
     // 第三方插件配置
     pluginOptions: {
