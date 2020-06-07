11ddcf97822c991faa1c99eb8654946d88042336
murphyyi <zy84338719@hotmail.com>
2020-06-08 03:01:33 +0800

    初始化

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+import company from '@/data/website/company'
+import test from '@/data/website/test'
+import pre from '@/data/website/pre'
+import prod from '@/data/website/prod'
+
+export default {
+    company,
+    test,
+    pre,
+    prod
+}
