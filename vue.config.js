f4a4304bd680db16f134533521d9789532a7259b
murphyyi <zy84338719@hotmail.com>
2020-06-08 13:07:56 +0800

    处理编译bug

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // 基本路径
     publicPath: './',
     // 输出文件目录
-    outputDir: 'public',
+    outputDir: 'dist',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
     // webpack配置
