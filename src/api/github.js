2d48258305eb80cb7a72cb6fa29e8703b50a2626
murphyyi <zy84338719@hotmail.com>
2021-03-08 02:21:14 +0800

    添加常用网址

--- src/api/github.js
+++ src/api/github.js
@@ -19,7 +19,7 @@ service.interceptors.response.use((resp) => {
 
 export function GetWebData() {
     return service({
-        url: "https://raw.githubusercontent.com/zy84338719/ework/zy/data/web.json",
+        url: "https://raw.githubusercontent.com/zy84338719/ework/data/web.json",
         method: "get",
     });
 }
